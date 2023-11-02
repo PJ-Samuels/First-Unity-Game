ddcb823120574fa99f2813aad2fa1fa57edfd6b1
PJ Samuels <osamuels@bu.edu>
2023-11-02 15:08:14 -0400

    inviisble wall fix and item interaction start

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,20 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask jumpableGround;
     private float dirx;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 7f;
     private enum MovementState {  idle, running, jumping, falling };
-    //private MovementState state = MovementState.idle;
+    private MovementState state = MovementState.idle;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -28,8 +31,9 @@ public class PlayerMovement : MonoBehaviour
         dirx = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirx*moveSpeed, rb.velocity.y);
 
-        if(Input.GetButton("Jump")){
-            rb.velocity = new Vector3(0, jumpForce, 0);
+        if(Input.GetButton("Jump") && IsGrounded()){
+
+            rb.velocity = new Vector2(0, jumpForce);
         }
 
         UpdateAnimationState();
@@ -59,5 +63,9 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.falling;
         }
         anim.SetInteger("state",(int)state);
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround );
     }
 }
